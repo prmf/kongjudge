e603854bff365ce98c867675840582ce95ceb392
Rodrigo Salazar <rodrodsalazar@gmail.com>
2011-11-05 22:14:27 -0400

    Changed KongJudge plan: Notes: -Ruby on rails -Will use Ideone API for now (Limited to 1000 free judges per month) -Initial Commit is just an 'empty' rails project (http://guides.rubyonrails.org/getting_started.html)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kongjudge::Application.config.secret_token = '553c40fba97171800de6c007938ab7455fe2a5d88f0a02a5fb79d6dd4526727287150e37d26d797f90b1e636f16bdb7944845dc2382596d20614f3f5c19b7699'
