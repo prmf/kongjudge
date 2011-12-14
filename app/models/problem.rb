f60f8de8b606ce7d72dcb48d585a90a314f10ef9
Rodrigo Salazar <rodrodsalazar@gmail.com>
2011-12-14 18:41:52 -0500

    Added field validation for all Problem data fields

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,12 +1,38 @@
 class Problem < ActiveRecord::Base
-      belongs_to :user
-      has_many :submissions
-      has_many :languages
-      
-      validates_format_of :problem_short_title, :with => /^[A-Z]+$/, :message => "must be uppercase with no whitespace"    
+	belongs_to :user
+	has_many :submissions
+	has_many :languages
 
-  def to_param
-    # http://api.rubyonrails.org/classes/ActiveRecord/Base.html#method-i-to_param
-    problem_short_title
-  end
+	#problem_title validation
+	validates_presence_of :problem_title, :message => "must exist"
+	validates_length_of :problem_title, :maximum=>50, :message => "must be within 50 characters"
+
+	#problem_short_title validation
+	validates_presence_of :problem_short_title
+	validates_uniqueness_of :problem_short_title, :message => "is already in use"      
+	validates_format_of :problem_short_title, :with => /^[A-Z0-9]+$/, :message => "must be uppercase alphanumeric with no whitespace"
+	validates_length_of :problem_short_title, :within => 3..8, :message => "must be within 3 and 8 alphanumeric characters long"    
+
+	#problem_statement validation
+	validates_presence_of :problem_statement, :message => "must not be blank"
+	validates_length_of :problem_statement, :maximum => 50000, :message => "too large"
+
+	#input validation
+	validates_length_of :input, :maximum => 1000000, :message => "too large"
+
+	#solution validation
+	validates_presence_of :solution, :message => "must exist"
+	validates_length_of :solution, :maximum => 1000000, :message => "too large"
+
+	#time_limit validation
+	validates_presence_of :time_limit
+	validates_numericality_of :time_limit, :only_integer => true
+	validates_inclusion_of :time_limit, :in => 1..300000
+
+	
+
+	def to_param
+		# http://api.rubyonrails.org/classes/ActiveRecord/Base.html#method-i-to_param
+		problem_short_title
+	end
 end
