4ec5d95ab16474dc56b94b74ee78bac96b5e3e18
Rodrigo Salazar <rodrodsalazar@gmail.com>
2011-12-14 17:29:06 -0500

    Updated problem_short_title error message on validation

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -3,7 +3,7 @@ class Problem < ActiveRecord::Base
       has_many :submissions
       has_many :languages
       
-      validates_format_of :problem_short_title, :with => /^[A-Z]+$/      
+      validates_format_of :problem_short_title, :with => /^[A-Z]+$/, :message => "must be uppercase with no whitespace"    
 
   def to_param
     # http://api.rubyonrails.org/classes/ActiveRecord/Base.html#method-i-to_param
