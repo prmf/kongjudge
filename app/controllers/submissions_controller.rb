f8f99be37017ee5aa5da1ea026b33f002a6ddc18
Rodrigo Salazar <rodrodsalazar@gmail.com>
2011-12-18 02:22:34 -0500

    Made some changes, but still very wrong

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,9 +1,11 @@
 class SubmissionsController < ApplicationController
 	def create
-		@curr_problem = @problem
+		@curr_problem = Problem.find_by_problem_short_title(params[:problem_short_title])
+
+		puts @curr_problem
 		@submission = @curr_problem.submissions.build(params[:submission])
 		if @submission.save
-			redirect_to (@curr_problem)
+			redirect_to (root_path)
 		else
 		 	render :new
 		end
