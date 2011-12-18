54639eefe191f5d0b64284a97797f00a93b9d08d
Rodrigo Salazar <rodrodsalazar@gmail.com>
2011-12-17 21:26:20 -0500

    Initial commit to submissions branch

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,11 +1,9 @@
 class SubmissionsController < ApplicationController
 	def create
-		problem_short_title = params[:submission][:problem_short_title]
-		@curr_problem = Problem.find_by_problem_short_title(problem_short_title)
-		@submission = @curr_problem.submissions.build(:code => params[:submission][:code], :user => current_user, :problem => @curr_problem)
-		@submission = current_user.submissions.build(:code => params[:submission][:code], :user => current_user, :problem => @curr_problem)
+		@curr_problem = @problem
+		@submission = @curr_problem.submissions.build(params[:submission])
 		if @submission.save
-			redirect_to (root_path)
+			redirect_to (@curr_problem)
 		else
 		 	render :new
 		end
