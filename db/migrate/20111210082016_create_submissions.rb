1c741e7e977c9b3bc00267f488f2b147fc23bbe1
Joseph Thuemler <joethuemler@gmail.com>
2011-12-18 22:52:36 -0500

    Added User and Problem foreign keys to the submissions table

--- db/migrate/20111210082016_create_submissions.rb
+++ db/migrate/20111210082016_create_submissions.rb
@@ -3,6 +3,8 @@ class CreateSubmissions < ActiveRecord::Migration
     create_table :submissions do |t|
       t.text :code
       t.string :result
+	  t.integer :user_id
+	  t.integer :problem_id
 
       t.timestamps
     end
