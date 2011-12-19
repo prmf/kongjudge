cd30fbeb0becd161014389d88ec65ea23151021a
Joseph Thuemler <joethuemler@gmail.com>
2011-12-18 22:52:36 -0500

    Added User and Problem foreign keys to the submissions table

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -2,7 +2,8 @@ class SubmissionsController < ApplicationController
 	def create
 		problem_short_title = params[:submission][:problem_short_title]
 		@curr_problem = Problem.find_by_problem_short_title(problem_short_title)
-		@submission = @curr_problem.submissions.build(:code => params[:submission][:code])
+		@submission = @curr_problem.submissions.build(:code => params[:submission][:code], :user => current_user, :problem => @curr_problem)
+		@submission = current_user.submissions.build(:code => params[:submission][:code], :user => current_user, :problem => @curr_problem)
 		if @submission.save
 			redirect_to (root_path)
 		else
