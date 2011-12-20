3459a9ca9fc3ea146c40ddd254e9837f7caff4e2
Joseph Thuemler <joethuemler@gmail.com>
2011-12-19 22:12:28 -0500

    Added a Submission#show command and view that works both for individual Submissions and Submissions by user. Not sure if this follows Rails conventions and if its a good idea to do this as I did, so this is in its own branch for now

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,4 +1,17 @@
 class SubmissionsController < ApplicationController
+	def show
+		if !!(params[:id] =~ /^[0-9]+$/) # this is checking to see if the id is an integer.
+			# if the ID is an integer, then we will display the submission that has an ID number of that integer
+			@submissions = Submission.find(params[:id])
+		else
+			# else, we display the submissions of the user with the id given
+			user = User.where(:username => params[:id])[0]
+			puts user.username
+			puts 'FINCHLEY CENTRAL!!!!!'
+			@submissions = user.submissions
+		end
+	end
+	
 	def create
 		problem_short_title = params[:submission][:problem_short_title]
 		@curr_problem = Problem.find_by_problem_short_title(problem_short_title)
